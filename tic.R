92dea80e570f3cbc8a3804b3c91c7f7b256c782f
earowang <earo.wang@gmail.com>
2018-12-25 23:30:31 +1100

    auto commit Tue Dec 25 23:30:31 AEDT 2018

--- tic.R
+++ tic.R
@@ -4,6 +4,9 @@ get_stage("before_install") %>%
 get_stage("install") %>%
   add_code_step(remotes::install_deps(dependencies = TRUE))
 
+get_stage("before_deploy") %>%
+  add_step(step_setup_ssh())
+
 get_stage("deploy") %>%
   add_code_step(
     call = {
@@ -31,41 +34,3 @@ get_stage("deploy") %>%
     }
   ) %>% 
   add_step(step_push_deploy())
-
-if (Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_code_step(
-      call = {
-        talks <- whisker::iteratelist(
-          gsub("^_site/", "", fs::dir_ls("_site/", type = "directory")),
-          value = "repo"
-        )
-
-        # usethis:::write_utf8
-        write_utf8 <- function (path, lines) {
-          stopifnot(is.character(path))
-          stopifnot(is.character(lines))
-          con <- file(path, encoding = "utf-8")
-          on.exit(close(con), add = TRUE)
-          if (length(lines) > 1) {
-            lines <- paste0(lines, "\n", collapse = "")
-          }
-          cat(lines, file = con, sep = "")
-          invisible(TRUE)
-        }
-
-        template <- readLines("README-template.md")
-        done <- strsplit(whisker::whisker.render(template), "\n")[[1]]
-        write_utf8("README.md", done)
-      }
-    ) %>% 
-    add_step(step_push_deploy())
-}
