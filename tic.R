26ff0bb55b31feee6682e41bfa3812c9a8dd3a3f
earowang <earo.wang@gmail.com>
2018-12-25 17:40:41 +1100

    added _redirects

--- tic.R
+++ tic.R
@@ -6,7 +6,7 @@ get_stage("install") %>%
 
 get_stage("deploy") %>%
   add_code_step(
-    prepare_call = {
+    call = {
       talks <- whisker::iteratelist(fs::dir_ls(type = "directory"), value = "repo")
 
       # usethis:::write_utf8
@@ -27,7 +27,8 @@ get_stage("deploy") %>%
       write_utf8("README.md", done)
     }
   ) %>% 
-  add_step(step_push_deploy())
+  add_step(step_setup_push_deploy(branch = "master", checkout = FALSE)) %>% 
+  add_step(step_do_push_deploy(commit_paths = "README.md"))
 
 if (Sys.getenv("id_rsa") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
@@ -39,5 +40,28 @@ if (Sys.getenv("id_rsa") != "") {
     add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
-    add_step(step_push_deploy())
+    add_code_step(
+      call = {
+        talks <- whisker::iteratelist(fs::dir_ls(type = "directory"), value = "repo")
+
+        # usethis:::write_utf8
+        write_utf8 <- function (path, lines) {
+          stopifnot(is.character(path))
+          stopifnot(is.character(lines))
+          con <- file(path, encoding = "utf-8")
+          on.exit(close(con), add = TRUE)
+          if (length(lines) > 1) {
+            lines <- paste0(lines, "\n", collapse = "")
+          }
+          cat(lines, file = con, sep = "")
+          invisible(TRUE)
+        }
+
+        template <- readLines("README-template.md")
+        done <- strsplit(whisker::whisker.render(template), "\n")[[1]]
+        write_utf8("README.md", done)
+      }
+    ) %>% 
+    add_step(step_setup_push_deploy(branch = "master", checkout = FALSE)) %>% 
+    add_step(step_do_push_deploy(commit_paths = "README.md"))
 }
