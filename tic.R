367e2fed7d786475ac7bf5dac3bf1b9d0395f72a
earowang <earo.wang@gmail.com>
2019-07-21 12:45:37 +1000

    auto commit Sun Jul 21 12:45:37 AEST 2019

--- tic.R
+++ tic.R
@@ -14,23 +14,9 @@ get_stage("deploy") %>%
         gsub("^_site/", "", fs::dir_ls("_site/", type = "directory")),
         value = "repo"
       )
-
-      # usethis:::write_utf8
-      write_utf8 <- function (path, lines) {
-        stopifnot(is.character(path))
-        stopifnot(is.character(lines))
-        con <- file(path, encoding = "utf-8")
-        on.exit(close(con), add = TRUE)
-        if (length(lines) > 1) {
-          lines <- paste0(lines, "\n", collapse = "")
-        }
-        cat(lines, file = con, sep = "")
-        invisible(TRUE)
-      }
-
       template <- readLines("README-template.md")
-      done <- strsplit(whisker::whisker.render(template), "\n")[[1]]
-      write_utf8("README.md", done)
+      done <- whisker::whisker.render(template)
+      usethis::write_over("README.md", done, quiet = TRUE)
     }
   ) %>% 
   add_step(step_push_deploy())
