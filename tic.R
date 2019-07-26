530ea4945f66523ff5d679ec3648de4cff36c851
earowang <earo.wang@gmail.com>
2019-07-26 23:47:20 +1000

    auto commit Fri Jul 26 23:47:20 AEST 2019

--- tic.R
+++ tic.R
@@ -14,9 +14,23 @@ get_stage("deploy") %>%
         gsub("^_site/", "", fs::dir_ls("_site/", type = "directory")),
         value = "repo"
       )
+
+      # usethis:::write_utf8
+      write_utf8 <- function (path, lines) {
+        stopifnot(is.character(path))
+        stopifnot(is.character(lines))
+        con <- file(path, encoding = "utf-8")
+        on.exit(close(con), add = TRUE)
+        if (length(lines) > 1) {
+          lines <- paste0(lines, "\n", collapse = "")
+        }
+        cat(lines, file = con, sep = "")
+        invisible(TRUE)
+      }
+
       template <- readLines("README-template.md")
-      done <- whisker::whisker.render(template)
-      usethis::write_over("~/README.md", done, quiet = TRUE)
+      done <- strsplit(whisker::whisker.render(template), "\n")[[1]]
+      write_utf8("README.md", done)
     }
   ) %>% 
   add_step(step_push_deploy())
